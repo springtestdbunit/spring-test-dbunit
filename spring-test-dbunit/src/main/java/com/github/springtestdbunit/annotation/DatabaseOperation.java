563749d41dfff6f9f0873f474cbc5c321fa1c28a
Michael Fazio <michaelfazio@me.com>
2012-11-19 20:13:35 +0800

    Added IdentityInsertOperation support.

--- spring-test-dbunit/src/main/java/com/github/springtestdbunit/annotation/DatabaseOperation.java
+++ spring-test-dbunit/src/main/java/com/github/springtestdbunit/annotation/DatabaseOperation.java
@@ -1,17 +1,15 @@
 /*
  * Copyright 2010 the original author or authors
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  */
 package com.github.springtestdbunit.annotation;
 
@@ -25,46 +23,56 @@ package com.github.springtestdbunit.annotation;
  */
 public enum DatabaseOperation {
 
-	/**
-	 * Updates the contents of existing database tables from the dataset.
-	 */
-	UPDATE,
+  /**
+   * Updates the contents of existing database tables from the dataset.
+   */
+  UPDATE,
 
-	/**
-	 * Inserts new database tables and contents from the dataset.
-	 */
-	INSERT,
+  /**
+   * Inserts new database tables and contents from the dataset.
+   */
+  INSERT,
 
-	/**
-	 * Refresh the contents of existing database tables. Rows from the dataset will insert or replace existing data. Any
-	 * database rows that are not in the dataset remain unaffected.
-	 */
-	REFRESH,
+  /**
+   * Refresh the contents of existing database tables. Rows from the dataset will insert or replace
+   * existing data. Any database rows that are not in the dataset remain unaffected.
+   */
+  REFRESH,
 
-	/**
-	 * Deletes database table rows that matches rows from the dataset.
-	 */
-	DELETE,
+  /**
+   * Deletes database table rows that matches rows from the dataset.
+   */
+  DELETE,
 
-	/**
-	 * Deletes all rows from a database table when the table is specified in the dataset. Tables in the database but not
-	 * in the dataset remain unaffected.
-	 * @see #TRUNCATE_TABLE
-	 */
-	DELETE_ALL,
+  /**
+   * Deletes all rows from a database table when the table is specified in the dataset. Tables in
+   * the database but not in the dataset remain unaffected.
+   * 
+   * @see #TRUNCATE_TABLE
+   */
+  DELETE_ALL,
 
-	/**
-	 * Deletes all rows from a database table when the table is specified in the dataset. Tables in the database but not
-	 * in the dataset are unaffected. Identical to {@link #DELETE_ALL} expect this operation cannot be rolled back and
-	 * is supported by less database vendors.
-	 * @see #DELETE_ALL
-	 */
-	TRUNCATE_TABLE,
+  /**
+   * Deletes all rows from a database table when the table is specified in the dataset. Tables in
+   * the database but not in the dataset are unaffected. Identical to {@link #DELETE_ALL} expect
+   * this operation cannot be rolled back and is supported by less database vendors.
+   * 
+   * @see #DELETE_ALL
+   */
+  TRUNCATE_TABLE,
 
-	/**
-	 * Deletes all rows from a database table when the tables is specified in the dataset and subsequently insert new
-	 * contents. Equivalent to calling {@link #DELETE_ALL} followed by {@link #INSERT}.
-	 */
-	CLEAN_INSERT;
+  /**
+   * Deletes all rows from a database table when the tables is specified in the dataset and
+   * subsequently insert new contents. Equivalent to calling {@link #DELETE_ALL} followed by
+   * {@link #INSERT}.
+   */
+  CLEAN_INSERT,
+
+  /**
+   * Provides the same functionality as {@link #CLEAN_INSERT}, however this operation also enables
+   * the inserting on identity columns under MS SQL Server (i.e. sets {@code 'SET IDENTITY_INSERT
+   * (table) ON')}.
+   */
+  MS_SQL_CLEAN_INSERT;
 
 }

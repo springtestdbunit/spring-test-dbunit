c39dd26c0108e6c3300321308e8b777a9b7d4f03
yangjianzhou <yangjianzhouctgu@126.com>
2017-11-18 12:11:45 +0800

    add XlsDataSetLoader and test it

--- spring-test-dbunit-sample/src/test/java/com/github/springtestdbunit/sample/service/XlsDataLoaderTest.java
+++ spring-test-dbunit-sample/src/test/java/com/github/springtestdbunit/sample/service/XlsDataLoaderTest.java
@@ -1,9 +1,10 @@
 package com.github.springtestdbunit.sample.service;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
+import com.github.springtestdbunit.DbUnitTestExecutionListener;
+import com.github.springtestdbunit.annotation.DatabaseSetup;
+import com.github.springtestdbunit.annotation.DbUnitConfiguration;
+import com.github.springtestdbunit.dataset.XlsDataSetLoader;
+import com.github.springtestdbunit.sample.entity.Person;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,32 +13,25 @@ import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
-import com.github.springtestdbunit.annotation.ExpectedDatabase;
-import com.github.springtestdbunit.sample.entity.Person;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({"classpath:com/github/springtestdbunit/sample/service/applicationContext.xml"})
+@DbUnitConfiguration(dataSetLoader= XlsDataSetLoader.class)
 @TestExecutionListeners({ DependencyInjectionTestExecutionListener.class, DbUnitTestExecutionListener.class })
-public class PersonServiceTest {
+public class XlsDataLoaderTest {
 
 	@Autowired
 	private PersonService personService;
 
 	@Test
-	@DatabaseSetup("sampleData.xml")
+	@DatabaseSetup("sampleData.xls")
 	public void testFind() throws Exception {
-		List<Person> personList = this.personService.find("hil");
+		List<Person> personList = this.personService.find("wang");
 		assertEquals(1, personList.size());
-		assertEquals("Phillip", personList.get(0).getFirstName());
-	}
-
-	@Test
-	@DatabaseSetup("sampleData.xml")
-	@ExpectedDatabase("expectedData.xml")
-	public void testRemove() throws Exception {
-		this.personService.remove(1);
+		assertEquals("wu", personList.get(0).getLastName());
 	}
 
 }
